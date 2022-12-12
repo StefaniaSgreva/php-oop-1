07cb5d4922721149a9640ffec188fe7b57ea9479
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-12 15:53:51 +0100

    step 1

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,7 +1,34 @@
 <?php
 
+//classe
 class Movie
 {
+    //variabili d'istanza - VARIABLES
+    public $title;
+
+    public $year;
+    public $duration;
+    public $genre;
+    public $description;
+    // public $poster;
+
+    //costruttore - CONSTRUCTOR
+    function __construct(string $_title, string $_year, string $_duration, string $_genre, string $_description)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->duration = $_duration;
+        $this->genre = $_genre;
+        $this->description = $_description;
+        // $this->poster = $_poster;
+    }
+
+    //metodi - METHODS
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
 }
 
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+// richiamo i file contenenti le CLASSI di oggetti 
 include_once __DIR__ . '/Models/Movie.php';
 
-$titolo = new Movie();
+//oggetti 'Movie' istanziati
+$firstMovie = new Movie('Encanto', '2021', '1h 52min', 'Famiglia, Fantasy, Animazione, Musical', 'Creato da Walt Disney Animation Studios con una colonna sonora che include nuove canzoni del premiato autore Lin-Manuel Mirando, "Encanto" racconta la storia della straordinaria famiglia dei Madrigal, che vive in una casa magica in Colombia. Mirabel è l unica figlia senza poteri, ma quando scopre che la magia della casa è in pericolo potrebbe rivelarsi l ultima speranza della famiglia.');
+$secondMovie = new Movie('Coco', '2017', '1h 44min', 'Famiglia, Fantasy, Animazione, Musical', 'La straordinaria avventura di un ragazzo che sogna di diventare un grande musicista e inizia un viaggio per scoprire i misteri celati dietro le storie e le tradizioni dei suoi antenati.');
+var_dump($firstMovie, $secondMovie);
 
-echo "prova";
+//stampare proprietà a schermo
+echo $firstMovie->getTitle();
+echo $secondMovie->getTitle();
+
+
 ?>
